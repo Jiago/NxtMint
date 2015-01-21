56d2fe0e3b7819e196cecd64ec0abeb4bb9524d1
ScripterRon <Ronald.Hoffman6@gmail.com>
2015-01-21 12:54:42 -0500

    Package Aparapi runtime with NxtMint

--- sample.mint.bat
+++ sample.mint.bat
@@ -1,3 +1,3 @@
 @REM Start NxtMint
 @echo Starting NxtMint
-java -Xmx256m -jar NxtMint-1.1.0.jar
+java -Xmx256m -Djava.library.path=aparapi -jar NxtMint-1.1.0.jar
--- sample.mint.sh
+++ sample.mint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start NxtMint
 echo "Starting NxtMint"
-java -Xmx256m -jar Nxt-1.1.0.jar
+java -Xmx256m -Djava.library.path=aparapi -jar Nxt-1.1.0.jar
